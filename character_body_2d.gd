3ce04642b0732096383721197f0eb4a22c775e4e
DDbrother <derekyin42009@gmail.com>
2025-09-27 10:00:16 -0400

    add movement

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -7,19 +7,23 @@ const JUMP_VELOCITY = -1100.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	#if not is_on_floor():
+		#velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	#if Input.is_action_just_pressed("jump") and is_on_floor():
+	#	velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	var xdirection := Input.get_axis("left", "right")
+	var ydirection := Input.get_axis("up", "down")
 
+	velocity.x = xdirection * SPEED
+	velocity.y = ydirection * SPEED
+
+	if xdirection == 0:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if ydirection == 0:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 	move_and_slide()
